77770608ca720b7e80d373b4707a3bf93ba4b580
Hendryboyz <Hendry.js1247@gmail.com>
2021-01-05 14:47:28 +0800

    feat. provide default time provider

---
+++ SimpleMFA.Net.Core/Providers/DefaultTimeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleMFA.Net.Core.Providers
+{
+    public class DefaultTimeProvider : ITimeProvider
+    {
+        public long GetNowTimeStamps()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        }
+    }
+}
