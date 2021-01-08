a291113fe0463ee670a84be68a972206c0929a70
Hendryboyz <Hendry.js1247@gmail.com>
2021-01-08 16:45:00 +0800

    feat. helper to generate QRCode for authenticator

---
+++ SimpleMFA.Net.Core/Helpers/SharedSecretHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace SimpleMFA.Net.Core.Helpers
+{
+    public static class SharedSecretHelper
+    {
+        public static string GenerateQRCodeURL(string secret, string username, string title = "")
+        {
+            string url = "https://chart.googleapis.com/chart?chs=200x200&chld=M|0&cht=qr&chl=";
+            string otpUrl = HttpUtility.UrlEncode($"otpauth://totp/{username}?secret={secret}");
+            if (!string.IsNullOrEmpty(title))
+            {
+                otpUrl += HttpUtility.UrlEncode("&issuer=" + HttpUtility.UrlEncode(title));
+            }
+            return url + otpUrl;
+        }
+    }
+}
